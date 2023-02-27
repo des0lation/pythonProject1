8a2ba886ae1a1d9e272cea9b45ad5751cf2e3a45
des0lation <72014077+des0lation@users.noreply.github.com>
2023-02-27 15:45:11 +1100

    Adding Version Strings

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
-import requests
 import time
-
-data = requests.get("http://127.0.0.1:8000/versions").text
-versions = data.strip("\"").split("\\nV")[1:]
-new_versions = []
-
+from requests import get
+import threading,math,time
+data = list(set(get("http://127.0.0.1:8000/versions").text[1:-1].split("\\nV")[1:]))
 t_1 = time.perf_counter()
-versions = set(versions)
-print("Searching set reduced to",len(versions))
-previous_version = tuple([0,0,0])
-for i in versions:
-    i = tuple(map(int, i.split(".")))
-    if i > previous_version:
-        new_versions.append(i)
-        previous_version = i
+maxs = []
+def sort_versions(data_chunk):
+    maxs.append(max(tuple(tuple(int(x) for x in version.split(".")) for version in data_chunk)))
+    return
 
-maxs = max(new_versions)
+data_len = len(data)
+x = 300
+threads = []
+
+for i in range(0,x):
+    start = math.floor(i*len(data)/x)
+    end = min(math.floor((i+1)*len(data)/x),data_len)
+    thread = threading.Thread(target = sort_versions ,args = [data[start:end]])
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+print(max(set(maxs)))
 t_2 = time.perf_counter()
-print(maxs,"Time Taken is",t_2 - t_1)
+print("Time Taken is",t_2 - t_1)
+
